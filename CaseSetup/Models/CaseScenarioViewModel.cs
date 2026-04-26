d1facf08b02e286ad2c04561bab7d5a2ede23a0a
Duckman <186611@stud.hvl.no>
2026-04-26 21:29:59 +0200

    update ducklib

---
+++ CaseSetup/Models/CaseScenarioViewModel.cs
@@ -0,0 +1,18 @@
+﻿
+
+using DuckLib;
+
+namespace CaseSetup.Models
+{
+    public class CaseScenarioViewModel
+    {
+        public int Id { get; set; }
+
+        public string CaseTitle { get; set; } = "";
+        public Patient Patient { get; set; } = new();  // oooo new() 
+
+
+        public bool StudentEditable { get; set; } = true;
+        bool IsActive { get; set; }
+    }
+}
---
+++ DuckLib/SimulationCase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DuckLib
+{
+    public class SimulationCase
+    {
+        public int Id { get; set; } = 0;
+
+        // case info
+        public string Title { get; set; } = "";
+        public string Description { get; set; } = "";
+
+        // simulation content
+        public Patient Patient { get; set; } = new();
+        public Vitals StartVitals { get; set; } = new();
+        public string LabValues { get; set; } = "";
+
+        // simulation goals
+        public string Goals { get; set; } = "";
+        public int GoalTimeMinutes { get; set; } = 15;
+
+        // simulation config
+        public bool StudentEditable { get; set; } = true;
+        public bool IsActive { get; set; } = false;
+
+        // meta?
+        public string CreatedByRole { get; set; } = "Student";
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+    }
+}
