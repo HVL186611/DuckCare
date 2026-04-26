946ac0fc00c07daa60e469f34822976e6c54d0a6
Duckman <186611@stud.hvl.no>
2026-04-26 21:33:26 +0200

    fix types and add datetime

--- DuckLib/Medication.cs
+++ DuckLib/Medication.cs
@@ -7,10 +7,10 @@ namespace DuckLib
     public class Medication
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Dose {  get; set; }
-        public string Route {  get; set; }
-        public string Frequency { get; set; }
-        public string Notes { get; set; }
+        public string Name { get; set; } = "";
+        public string Dose {  get; set; } = "";
+        public string Route {  get; set; } = "";
+        public string Frequency { get; set; } = "";
+        public string Notes { get; set; } = "";
     }
 }
