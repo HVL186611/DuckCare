7bf9fe073eb79afeba4c4d03cbe94c71f19d4431
Daniella Stensland <dellasten25@gmail.com>
2026-04-26 20:41:25 +0200

    add allergy, medication, patient, vitals and stub for labvalue classes

---
+++ DuckLib/Medication.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DuckLib
+{
+    public class Medication
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Dose {  get; set; }
+        public string Route {  get; set; }
+        public string Frequency { get; set; }
+        public string notes { get; set; }
+    }
+}
