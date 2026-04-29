8d7d2451efb6fbc9976e4bc95b82cbdc0f50d836
Daniella Stensland <dellasten25@gmail.com>
2026-04-29 14:44:46 +0200

    partial work, commit so others can do work

---
+++ DuckLib/Goal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DuckLib
+{
+    public class Goal
+    {
+        public string Description { get; set; } = "";
+        public int TimerMinutes { get; set; }
+
+        public (int Min, int Max)? SystolicBP { get; set; }
+        public (int Min, int Max)? DiastolicBP { get; set; }
+        public (int Min, int Max)? HeartRate {  get; set; }
+        public (int Min, int Max)? RespiratoryRate {  get; set; }
+        public (double Min, double Max)? SpO2 { get; set; }
+        public (double Min, double Max)? Temperature { get; set; }
+    }
+}
