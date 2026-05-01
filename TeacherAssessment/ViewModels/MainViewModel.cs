fd5880f23ad507237be11b8490d5f0bf3d350586
HVL186618 <186618@stud.hvl.no>
2026-05-01 12:06:26 +0200

    Push

---
+++ TeacherAssessment/ViewModels/MainViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace TeacherAssessment.ViewModels
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        public partial ObservableCollection<string> EventLog { get; set; } = new();
+    }
+}
